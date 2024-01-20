bd1040193deaa5eb6e5751e338dd96c3cb8efb47
BeubeuReturns <139000928+BeubeuReturns@users.noreply.github.com>
2024-01-20 15:31:04 +1100

    back to dist

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/<REPO>/',
-  build: {
-    outDir: 'docs',
-  },
   plugins: [
     vue(),
     vueJsx(),
