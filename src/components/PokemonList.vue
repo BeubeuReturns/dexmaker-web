22198fe76a86a222ed6f2c49f9b97563e9b48018
BeubeuReturns <139000928+BeubeuReturns@users.noreply.github.com>
2024-01-20 15:09:53 +1100

    First commit

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <ul>
+        <li v-for="pokemon in pokemons" :key="pokemon.name">
+          {{ pokemon.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        pokemons: []
+      };
+    },
+    mounted() {
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+        .then(response => response.json())
+        .then(data => {
+          this.pokemons = data.results;
+        });
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
