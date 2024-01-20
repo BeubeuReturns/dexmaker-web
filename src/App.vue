50e8a9a2d418a4d46aa22dbad18720a772e09bb3
BeubeuReturns <139000928+BeubeuReturns@users.noreply.github.com>
2024-01-20 22:05:32 +1100

    Fixed display

--- src/App.vue
+++ src/App.vue
@@ -5,89 +5,48 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div class="app-container">
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="wrapper">
+        <HelloWorld msg="Regional Pokedex Maker" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
 
-  <RouterView />
-</div>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
 .app-container {
   display: flex;
   flex-direction: column;
-  width:max-content;
+  width: max-content;
 }
 
 header {
   line-height: 1.5;
-  width:100%;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+header {
+  /* Your header styles */
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.main-content {
+  display: flex;
+  width: 100%;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+@media (min-width: 768px) {
+  .main-content {
+    /* Larger screens can have a row-based layout */
+    flex-direction: row;
   }
 }
 </style>
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,38 +1,43 @@
 <template>
   <div class="content-container">
-  <div class="pokemon-list">
-    <div
-      v-for="pokemon in pokemons"
-      :key="pokemon.id"
-      class="pokemon-item"
-      @click="selectPokemon(pokemon)"
-    >
-      <img :src="`/dexmaker-web/images/pokemons/${pokemon.id}.png`" :alt="pokemon.name">
-      <p>{{ pokemon.name }}</p>
+    <div class="pokemon-list">
+      <div
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        class="pokemon-item"
+        @click="selectPokemon(pokemon)"
+      >
+        <img :src="`/dexmaker-web/images/pokemons/${pokemon.id}.png`" :alt="pokemon.name" />
+        <p>{{ formatName(pokemon.name) }}</p>
+      </div>
     </div>
-  </div>
-  <div v-if="selectedPokemon" class="pokemon-details">
-    <h2>{{ selectedPokemon.name }}</h2>
-    <ul>
-      <h3>Abilities :</h3>
-      <li v-for="ability in selectedPokemon.normal_abilities" :key="ability">
-        {{ ability }}: {{ getAbilityFlavorText(ability) }}
-      </li>
-      <h3>Hidden Ability :</h3>
-      <li v-for="ability in selectedPokemon.hidden_abilities" :key="ability">
-        {{ ability }}: {{ getAbilityFlavorText(ability) }}
-      </li>
-    </ul>
-    <h3>Stats :</h3>
-    <div class="stat" v-for="(value, key) in selectedPokemon.stats" :key="key">
-    <span class="stat-name">{{ getStatName(key) }}:</span>
-    <span class="stat-value">{{ value }}</span>
-    <div class="bar-container">
-      <div class="bar" :style="{ width: getBarWidth(value), backgroundColor: getStatColor(key) }"></div>
+    <div v-if="selectedPokemon" class="pokemon-details">
+      <h2>{{ formatName(selectedPokemon.name) }}</h2>
+      <ul>
+        <h3>Abilities:</h3>
+        <li v-for="ability in selectedPokemon.normal_abilities" :key="ability">
+          {{ formatAbility(ability) }}: {{ getAbilityFlavorText(ability) }}
+        </li>
+        <li
+          v-for="ability in selectedPokemon.hidden_abilities"
+          :key="ability"
+          class="hidden-ability"
+        >
+          {{ formatAbility(ability) }}: {{ getAbilityFlavorText(ability) }}
+        </li>
+      </ul>
+      <h3>Stats :</h3>
+      <div class="stat" v-for="(value, key) in selectedPokemon.stats" :key="key">
+        <span class="stat-name">{{ getStatName(key) }}:</span>
+        <span class="stat-value">{{ value }}</span>
+        <div class="bar-container">
+          <div
+            class="bar"
+            :style="{ width: getBarWidth(value), backgroundColor: getStatColor(key) }"
+          ></div>
+        </div>
+      </div>
     </div>
-      <span>{{ value }}</span>
-    </div>
-  </div>
   </div>
 </template>
 
@@ -42,30 +47,29 @@ export default {
     return {
       pokemons: [],
       abilitiesFlavorText: {},
-      selectedPokemon: null,
-    };
+      selectedPokemon: null
+    }
   },
   mounted() {
     fetch('/dexmaker-web/pokemon_data_sorted.json')
-      .then(response => response.json())
-      .then(data => {
-        this.pokemons = data;
-      });
+      .then((response) => response.json())
+      .then((data) => {
+        this.pokemons = data
+      })
 
     fetch('/dexmaker-web/abilities_flavor_text.json')
-      .then(response => response.json())
-      .then(data => {
-        this.abilitiesFlavorText = data;
-      });
+      .then((response) => response.json())
+      .then((data) => {
+        this.abilitiesFlavorText = data
+      })
   },
   methods: {
     selectPokemon(pokemon) {
-      this.selectedPokemon = pokemon;
+      this.selectedPokemon = pokemon
     },
     getAbilityFlavorText(ability) {
       console.log(this.abilitiesFlavorText[ability]?.en)
-      return this.abilitiesFlavorText[ability]?.en || 'No description available.';
-      
+      return this.abilitiesFlavorText[ability]?.en || 'No description available.'
     },
     getStatColor(stat) {
       const colors = {
@@ -75,13 +79,13 @@ export default {
         'special-attack': 'green',
         'special-defense': 'blue',
         speed: 'purple'
-      };
-      return colors[stat] || 'grey';
+      }
+      return colors[stat] || 'grey'
     },
     getBarWidth(value) {
-      const maxStatValue = 200;
-      const widthPercentage = (value / maxStatValue) * 100;
-      return `${widthPercentage}%`;
+      const maxStatValue = 200
+      const widthPercentage = (value / maxStatValue) * 100
+      return `${widthPercentage}%`
     },
     getStatName(key) {
       const names = {
@@ -91,17 +95,23 @@ export default {
         'special-attack': 'Spe Atk',
         'special-defense': 'Spe Def',
         speed: 'Speed'
-      };
-      return names[key] || key;
+      }
+      return names[key] || key
     },
-  },
-};
+    formatName(name) {
+      return name.charAt(0).toUpperCase() + name.slice(1)
+    },
+    formatAbility(ability) {
+      return ability
+        .split('-')
+        .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+        .join(' ')
+    }
+  }
+}
 </script>
 
-
-
 <style scoped>
-
 .pokemon-stats .stat {
   display: flex;
   align-items: center;
@@ -114,33 +124,60 @@ export default {
 .bar-container {
   height: 10px; /* Adjust as needed */
   background-color: transparent; /* Light grey background for the bar */
-  border-radius: 5px; /* Rounded corners for the bar container */
+  border-radius: 2px; /* Rounded corners for the bar container */
 }
 
-.bar {
-  height: 100%;
-  border-radius: 5px; /* Optionally rounded corners for the bar itself */
-  /* Transition for animated width change */
-  transition: width 0.5s ease;
-}
 
 .stat {
   display: grid;
-  grid-template-columns: auto auto 1fr;
+  grid-template-columns: minmax(auto, 100px) minmax(auto, 50px) auto; /* Adjust as needed */
   align-items: center;
   gap: 0.5rem;
 }
+
+.stat-name,
+.stat-value {
+  text-align: right; /* Align text to the right */
+}
+
+.bar-container {
+  width: 100%; /* Take up remaining space */
+  background-color: transparent;
+  border-radius: 5px;
+  overflow: hidden; /* Ensures inner bar respects border-radius */
+}
+
+.bar {
+  width: 0; /* Initial width */
+  height: 100%;
+  border-radius: 5px; /* Optionally rounded corners for the bar itself */
+  transition: width 0.5s ease; /* Smooth transition for bar width */
+}
+
 .content-container {
   display: flex;
-  width: 100%; /* Take the full viewport width */
+  flex-direction: column; /* Stack elements vertically on smaller screens */
+  width: 98vw; /* Ensure it spans the full viewport width */
+  min-height: 100vh; /* Use at least the full viewport height */
 }
 
 .pokemon-list {
-  width: 40%; /* Take 40% of the content-container's width */
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr)); /* Adjust the minmax values as needed */
-  gap: 10px; /* Space between items */
-  overflow-y: auto; /* Add scrolling if content overflows */
+  grid-template-columns: repeat(auto-fill, minmax(96px, 1fr));
+  padding: 0.5rem; /* Add padding around the grid */
+  width: 100%; /* Use full width on smaller screens */
+}
+
+.pokemon-details {
+  width: 100%; /* Use full width on smaller screens */
+  padding: 1rem; /* Add padding for consistency */
+}
+
+/* Media query for larger screens */
+@media (min-width: 768px) {
+  .content-container {
+    flex-direction: row; /* Side-by-side layout on larger screens */
+  }
 }
 
 .pokemon-item {
@@ -149,14 +186,16 @@ export default {
 }
 
 .pokemon-item img {
-  width: 100%; /* Images will fill their grid item */
+  width: 80%;
   height: auto;
 }
 
+
 .pokemon-details {
-  width: 60%; /* Remaining space for details */
-  /* Additional styles for details */
+  width: 40%; /* Fill the remaining 40% of the page */
+}
+
+.hidden-ability {
+  font-style: italic;
 }
 </style>
-
-
